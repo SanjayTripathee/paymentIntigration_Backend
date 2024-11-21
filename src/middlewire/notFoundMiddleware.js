f924c7ff91f0c63925911ddf78dd5d09245a340f
sanjay <st1706851@gmail.com>
2024-11-21 14:15:12 +0545

    middlewire for authorized , notfoundMiddlewire,isAuthentication complete

---
+++ src/middlewire/notFoundMiddleware.js
@@ -0,0 +1,9 @@
+const notFoundMiddleware = (req, res, next) => {
+    res.status(404).json({
+      sucess: false,
+      message: "Route not Found",
+    });
+  };
+  
+  export default notFoundMiddleware;
+  
