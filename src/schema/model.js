cb58601e51f6e29672fbf6ae0a516260435d0051
sanjay <st1706851@gmail.com>
2024-11-22 13:31:02 +0545

    model of product define

--- src/schema/model.js
+++ src/schema/model.js
@@ -1,6 +1,8 @@
 import { model } from "mongoose";
 import webUserSchema from "./webUserSchema.js";
+import productSchema from "./productSchema.js";
 
 
 export let WebUser = model("WebUser", webUserSchema);
+export let Product = model("Product", productSchema);
 
