81e2aa93142c9ba9007a0133e317607ec2c361a7
sanjay <st1706851@gmail.com>
2024-11-22 13:39:16 +0545

    router of product also define

---
+++ src/routes/productRouter.js
@@ -0,0 +1,15 @@
+import { Router } from "express";
+import { crateProductController, deleteProductController, readAllProductController, readSpecificProductController, updateProductController } from "../controller/productController";
+
+
+let productRouter = Router();
+productRouter
+  .route("/")
+  .post(crateProductController)
+  .get(readAllProductController);
+productRouter
+  .route("/:id")
+  .get(readSpecificProductController)
+  .patch(updateProductController)
+  .delete(deleteProductController);
+export default productRouter;
