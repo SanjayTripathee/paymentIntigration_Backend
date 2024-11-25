34cb9c8a777c165b4f75f552639c097b8f3ec0f2
sanjay <st1706851@gmail.com>
2024-11-25 15:33:16 +0545

    some correction and updation

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ import errorMiddleware from "./src/middlewire/errorMiddlewire.js";
 import notFoundMiddleware from "./src/middlewire/notFoundMiddleware.js";
 import webUserRouter from "./src/routes/webUserRouter.js";
 import productRouter from "./src/routes/productRouter.js";
-import fileRouter from "./src/routes/fileRouter.js";
 import router from "./src/routes/paymentRoute.js";
+import fileRouter from "./src/routes/fileRouter.js";
 
 let expressApp = express();
 expressApp.use(cors()); //! cors is enable so that we could hit api from browser(it is always put on top)
@@ -19,8 +19,11 @@ expressApp.listen(8000, () => {
 
 expressApp.use("/web-users", webUserRouter); //write kabab case on sending part ok
 expressApp.use("/product", productRouter);
-expressApp.use("/file", fileRouter);
 expressApp.use("/payment", router);
+
+expressApp.use("/file", fileRouter);
+
+expressApp.use(express.static("./public"));
 
 expressApp.use("*", notFoundMiddleware); //! always put it after all route is decleare ..put down after route ok...
 
